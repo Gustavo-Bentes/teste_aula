a6140885617f1549591e34f52527ca0d78411c74
Gustavo-Bentes <gustavobentes05@gmail.com>
2022-12-13 09:19:27 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 10;
+        var resultado = "";
+        if(numero >= 0){
+            resultado = "positivo";
+        }else{
+            resultado = "negativo";
+        }
+
+
+
+
+        System.out.println("Hello, World! O numero e "+resultado);
+    }
+}
